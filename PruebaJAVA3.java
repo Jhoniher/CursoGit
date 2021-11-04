8ef2a64d48494fde1ca52b3567677f337946580b
Jhony Alpala <jhalpalaa@unal.edu.co>
2021-11-04 17:51:29 -0500

    Agregadas variables a b y c

---
+++ PruebaJAVA3.java
@@ -0,0 +1,15 @@
+
+public class PruebaJAVA3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a,b,c;
+		a=2;
+		b=3;
+		c=2;
+		System.out.println(a+b+c);
+		
+
+	}
+
+}
